08bb3e2aa312d6a4666a52d405b691be7ec0f654
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-05 07:40:28 +0000

    Deploy beldmian/covid-tracker to github.com/beldmian/covid-tracker.git:gh-pages

---
+++ precache-manifest.3797aad38a30987b0c2c4ee622d01c72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1619a0fbe81a2893ed534d8c5f6b9e2f",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "36ac6795337c16ee146b",
+    "url": "/covid-tracker/static/css/main.9e025cfa.chunk.css"
+  },
+  {
+    "revision": "5c4674602f55a4660298",
+    "url": "/covid-tracker/static/js/2.3e4ec223.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid-tracker/static/js/2.3e4ec223.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36ac6795337c16ee146b",
+    "url": "/covid-tracker/static/js/main.3ad63639.chunk.js"
+  },
+  {
+    "revision": "7235eac3c1cb45719e76",
+    "url": "/covid-tracker/static/js/runtime-main.ac391b8e.js"
+  }
+]);
